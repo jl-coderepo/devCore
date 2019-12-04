bb0dd893592a965112c7a379bcf076fa8efa861e
June <june87lee@gmail.com>
2019-12-03 19:00:28 -0800

    0.0.1, 02: Add schemas, models, middleware, routes
      Following SCRUM, current sprint involved finishing up defining schemas,
    creating models, implement a simple authentication middleware, and create
    a simple RESTful api which the front-end can interact with. Everything is
    subject to change. Currently trying to launch a barebones site.

---
+++ config/db.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+const config = require("config");
+const db = config.get("mongoURI");
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(db, {
+      useNewUrlParser: true,
+      useCreateIndex: true,
+      useUnifiedTopology: true,
+      useFindAndModify: false
+    });
+    console.log("  __DB: database connected");
+  } catch (err) {
+    console.error(`  __DB: ${err.message}`);
+    // Exit process with failure if database connection cannot be established
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
